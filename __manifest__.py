185c603cd8dfda65b93ece132946da13a3649153
lionizedevelopers <lionizedevelopers@gmail.com>
2023-11-23 21:51:26 +0530

    [17.0][NEW]  hr employee bulk document upload module create

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,7 @@
 {
     'name': 'HR Documents',
-    'description    ': """HR Documents """,
+    'description    ': """Upload Zip file of bulk Employee and Profile image at a time using HR Documents module.
+                          By:lionizedevelopers@gmail.com""",
     'version': '17.0.1.0',
     'category': 'Human Resources',
     "sequence": 1,
